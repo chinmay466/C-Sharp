e514ea45039dd85a3b3b84aee489e41b6e95f53d
Chinmay_.04 <chinmaypoojary04@gmail.com>
2025-10-23 14:21:14 +0530

    Create abstract.cs

---
+++ abstract.cs
@@ -0,0 +1,29 @@
+﻿namespace AbstractCalculateShapeArea
+{
+    using System;
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+    class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape c1 = new Circle(5.0);
+            Console.WriteLine("Area of the circle: " + c1.CalculateArea());
+
+        }
+    }
+}
